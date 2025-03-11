0fee76e61d6a25eb1d46d95715a79ba15cd16003
ZubiaAmeer <zubiaameer0@gmail.com>
2025-03-11 11:23:24 -0700

    Add files via upload

---
+++ pointer 1.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int *ptr=NULL;
+	cout<<"the value of pointer is"<<"="<<ptr<<endl;
+	int num;
+	cout<<"enter the number"<<endl;
+	cin>>num;
+	ptr=&num;
+	cout<<"the adress of num is"<<ptr<<endl;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
---
+++ pointer 2.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a=1;
+	char b='+';
+	float c=1.999;
+	double d=123.67890;  
+    void *p=&a;
+	cout<<"the adress of a is"<<p<<endl;
+    p=&b; 
+    cout<<"the adress of b is"<<p<<endl;
+	p=&c;
+	cout<<"the adress of c is"<<p<<endl;
+	p=&d;
+	cout<<"the adress of d is"<<p<<endl;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
